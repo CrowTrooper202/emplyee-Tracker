276338daecedde3ff7f404649c920773bddb8f58
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-20 21:55:53 -0400

    added seed code

---
+++ seed.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (id, name)
+VALUES
+(1, 'Sales'),
+(2, 'Engineering'),
+(3, 'Security'),
+(4, 'Legal'),
+(5, 'Finance');
+
+INSERT INTO role (id, title, salary)
+VALUES
+(1, 'sales Lead', 27000, [])
+(1, 'Salesperson', 20000, [])
+(1, 'Lead Engineer', 146000, [])
+(1, 'Software Engineer',125000, [])
+(1, 'Account manager', 60000, [])
+(1, 'Legal Team Lead', 155000, [])
+(1, 'Lawyer', 134000, [])
+(1, 'Shift supervisor', 60000, [])
+(1, 'Security Guard', 50000, [])
+
+
+INSERT INTO employee (id, first_name, last_name,manager_id)
+VALUES
+(1, 'John', 'smith',[], 'Jane Doe')
+(1, 'Russel', 'Bell', [], 'Jane Doe')
+(1, 'Myra', 'Estrada', [], 'null')
+(1, 'Wilfrid', 'Hutchinson', [], 'Haley Banks')
+(1, 'Muriel', 'Bowman', [], 'null')
+(1, 'Wally', 'Sherman', [], 'Haley Banks')
+(1, 'Oliver', 'Heath', [], 'Jane Doe')
+(1, 'Pat', 'Gildon', [], 'null')
--- sever.js
+++ sever.js
@@ -22,7 +22,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
-// Query database
+
 db.query('SELECT * FROM students', function (err, results) {
   if (err) {
     throw err;
@@ -30,7 +30,7 @@ db.query('SELECT * FROM students', function (err, results) {
   console.log(results);
 });
 
-// Default response for any other request (Not Found)
+
 app.use((req, res) => {
   res.status(404).end();
 });
