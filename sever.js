1ac7cc4125d5b3f69ff5a3e311a86e37912a0997
CrowTrooper202 <adamb88cke@gmail.com>
2023-04-24 20:19:17 -0400

    tied index and sever and moved in inquirer to server

--- sever.js
+++ sever.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const mysql = require('mysql2');
-
+const inquirer = require('inquirer');
+const generateResponce = require('./index')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,24 +9,42 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-e
+
+
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    
-    user: 'root',
-    
-    password: 'GundamTrooper202',
-    database: 'company_db'
-  },
-  console.log(`Connected to the company_db database.`)
+    {
+        host: 'localhost',
+
+        user: 'root',
+
+        password: 'GundamTrooper202',
+        database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
 );
 
 
+inquirer
+    .createPromptModule([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'options',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        }
+    ])
+    .then((data) => {
+        generateResponce(data), (err) =>
+            err ? console.log(err) : console.log('success!')
+    })
+
+
+
+
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
